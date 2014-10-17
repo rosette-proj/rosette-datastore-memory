ea128a4d535c9f6f5ee410058f6066ccf70d0420
Cameron Dutro <camertron@gmail.com>
2014-10-17 10:32:48 -0700

    Removing some files

--- tester.rb
+++
@@ -1,7 +0,0 @@
-require 'rosette/data_stores/in_memory_data_store'
-require 'pry-nav'
-
-datastore = Rosette::DataStores::InMemoryDataStore.new
-
-datastore.add_or_update_commit_log('foobar', 'abc123')
-binding.pry
